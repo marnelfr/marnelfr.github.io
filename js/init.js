08f6102260137e3e1af5e740b90bcd585467a14e
marnelfr <gmlginolias@gmail.com>
2020-01-09 04:02:25 +0100

    testing js

--- js/init.js
+++ js/init.js
@@ -90,7 +90,7 @@ jQuery(document).ready(function($) {
 ------------------------------------------------------ */
    setTimeout( function() {
 
-	   $('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
+	   //$('h1.responsive-headline').fitText(1, { minFontSize: '40px', maxFontSize: '90px' });
 	   
    }, 100);
 
