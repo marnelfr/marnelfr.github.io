fca470a10c1c221c5e717a588f2a62bf0a57cfce
marnelfr <gmlginolias@gmail.com>
2020-01-21 03:27:01 +0100

    Done

--- js/app.js
+++ js/app.js
@@ -17,16 +17,167 @@ let db = firebase.database();
 /*let speach = db.ref('speach').set({
     current_page: 1
 });*/
-/*let pages2 = db.ref('speach/pages/1').set({
-    title: "Acceuil",
-    content: `<div style='display: table; height: 60%; width: 100%; text-align: center;'>
-        <span style='display: table-cell; vertical-align: middle;'>
-            <h1 style='color: #3c8dbc; font-size: 40px'>Accueil</h1>
-        </span>
-    </div>
+/*
+let pages2 = db.ref('speach/pages/1').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">STOCK EXCHANGE</h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-12 col-sm-12 col-xs-12">
+            <p style="color: #222222; font-style: italic">Presented by</p>
+            <p id="marnel">Marnel Gnacadja</p>
+        </div>
     `
 })
-let pages2 = db.ref('speach/pages/2').set({
+pages2 = db.ref('speach/pages/2').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Outline</h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-left">
+            <h3 style="font-weight: bold">Introduction</h3>
+            <h3>Chapter I: What is this?</h3>
+            <h3>Chapter II: Role of Stock Exchange</h3>
+            <h3>Chapter III: Risks for stock investors</h3>
+            <h3>Chapter IV: Major Stock Exchange</h3>
+            <h3>Recommendation</h3>
+            <h3 style="font-weight: bold">Conclusion</h3>
+        </div>
+    `
+})
+pages2 = db.ref('speach/pages/3').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Introduction</h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <p>
+                Knowing how to secure your financial well-being is very important. That’s why we are going to travel through the stock exchange.
+                <br>
+                <span style="font-weight: bold">What is a stock exchange? What is the purpose of a stock exchange?</span>
+                <br>Those are great questions.
+
+            </p>
+        </div>
+    `
+})
+pages2 = db.ref('speach/pages/4').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Chapter I <br><small>What is this?</small></h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <p>
+                A <span style="font-weight: bold">stock exchange</span> is an organized and regulated financial market where securities (bonds, stocks, shares) are bought and sold at prices governed by the forces of demand and supply
+            </p>
+        </div>
+    `
+})
+
+pages2 = db.ref('speach/pages/5').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Chapter II <br><small>Role of Stock Exchange</small></h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <p>
+                Stock exchanges play a vital role in the functioning of the economy by providing the backbone to a modern nation's economic infrastructure. Stock exchanges help companies raise money to expand. They also provide individuals the ability to invest in companies...
+            </p>
+        </div>
+    `
+})
+
+
+pages2 = db.ref('speach/pages/6').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Chapter III <br><small>Risks for stock investors</small></h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <p>
+                Investing in stocks is a risky business. There are some risks you have some control over and others that you can only guard against.
+                <br><br>
+                Thoughtful investment selections that meet your goals and risk profile keep individual stock and bond risks at an acceptable level.
+            </p>
+        </div>
+    `
+})
+
+
+pages2 = db.ref('speach/pages/7').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Chapter IV <br><small>Major Stock Exchanges</small></h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <ol>
+                <li>The New York Stock Exchange</li>
+                <li>Shanghai Stock Exchange</li>
+                <li>Tokyo Stock Exchange</li>
+                <li>Euronext</li>
+                <li>BRVM</li>
+                <li>etc.</li>
+            </ol>
+        </div>
+    `
+})
+pages2 = db.ref('speach/pages/8').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Recommendation</h1>
+            </span>
+            
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <p>
+                Stocks and bonds represent two different ways for an entity to save and raise money. Since stock may make you take risk, you may think there is no better way than invest in bonds.
+            </p>
+        </div>
+    `
+})
+pages2 = db.ref('speach/pages/9').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 style="color: #3c8dbc; font-size: 40px">Conclusion</h1>
+            </span>
+        </div>
+        <div style="margin-top:2em;" class="col-md-8 col-sm-8 col-xs-12 col-sm-offset-2 col-md-offset-2 text-center">
+            <p>
+                Stock market has its risk but can help you to achieve your financial goal. Many people invest in both stocks and bonds to diversify.
+                <br>
+                So do whatever you want but at least, invest in something, make your retirement more comfortable.
+            </p>
+        </div>
+    `
+})
+
+pages2 = db.ref('speach/pages/10').set({
+    title: "STOCK EXCHANGE",
+    content: `<div style="display: table; height: 60%; width: 100%; text-align: center;">
+            <span style="display: table-cell; vertical-align: middle;">
+                <h1 id="marnel" style="color: #3c8dbc; font-size: 40px">Thanks for your attention</h1>
+            </span>
+        </div>
+    `
+})
+*/
+/*let pages2 = db.ref('speach/pages/2').set({
     title: "Acceuil",
     content: `<div style='display: table; height: 60%; width: 100%; text-align: center;'>
         <span style='display: table-cell; vertical-align: middle;'>
@@ -81,9 +232,12 @@ let pages3 = db.ref('speach/pages/3').set({
                 $('#start').on('click', function (e) {
                     e.preventDefault()
                     loading($(this))
-                    db.ref('speach/timer').update({
-                        started: true,
-                        minute: 20
+                    db.ref('speach').update({
+                        current_page: 1,
+                        timer: {
+                            started: true,
+                            minute: 20
+                        }
                     })
                 })
                 //Manager btn
@@ -213,6 +367,9 @@ function counter(timer) {
 
     if (min === '00' && sec === '00') {
         clearInterval(interval)
+        setTimeout(function () {
+            timer.text('00:00:00')
+        }, 1000)
         return
     }
 
